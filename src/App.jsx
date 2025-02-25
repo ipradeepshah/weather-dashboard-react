e19661f270b142a1eb7a53a5708eb3c1d5a909ae
ipradeepshah <ipradeepshah@gmail.com>
2025-02-25 19:39:22 +0545

    added navbar and header of the weather app dashboard

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from './components/NavBar/NavBar';
+import Header from './components/Header/Header';
+
+const App
+ = () => {
+  return (
+    <>
+    <NavBar />
+    <Header/>
+    </>
+    
+  );
+}
+
+export default App
